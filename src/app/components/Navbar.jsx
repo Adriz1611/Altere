6e1fa5e5961d4b1216f32648635bc39493ca7171
AriaFantom <priyanshudebnath348@gmail.com>
2024-09-10 03:01:57 +0530

    Made the navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,63 @@
+"use client"
+import {motion} from "framer-motion"
+import Image from "next/image";
+import {useState} from "react";
+import Link from "next/link";
+
+const Navlinks = [
+    {
+        name: "Home",
+        link: "/"
+    },
+    {
+        name: "About",
+        link: "/"
+    },
+    {
+        name: "Contact",
+        link: "/"
+    },
+]
+
+export default function Navbar() {
+    const [activeIndex, setActiveIndex] = useState(0)
+    return (
+        <header className="py-6">
+            <center>
+                <nav className="border w-1/2 rounded-[2rem] flex flex-row items-center justify-between px-3">
+                    <Image src="/nav-log.png" alt="logo" height={150} width={150} className=" p-3"/>
+                    <div className="flex flex-row uppercase relative z-[100] ">
+                        {
+                            Navlinks.map((link, index) => {
+                                return (
+                                    <Link className="rounded-[2rem] items-center justify-center w-28 relative py-3 " key={index} href={link.link}
+                                          onClick={() => setActiveIndex(index)}><span>{link.name}</span>
+                                        {
+                                            index === activeIndex && (
+                                                <motion.div
+                                                    layoutId="navbar"
+                                                    aria-hidden="true"
+                                                    style={{
+                                                        width: "100%",
+                                                    }}
+                                                    transition={{
+                                                        type: "spring",
+                                                        bounce: 0.25,
+                                                        stiffness: 130,
+                                                        damping: 9,
+                                                        duration: 0.3,
+                                                    }}
+                                                    className="bg-green-500 absolute h-full rounded-[2rem] left-0 bottom-0 -z-10"/>
+                                            )
+                                        }
+                                    </Link>
+
+                                )
+                            })
+                        }
+                    </div>
+                </nav>
+            </center>
+        </header>
+    )
+}
