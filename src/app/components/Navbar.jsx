d75c3959386dcb8de72d9b6288d83fdfcede7943
AriaFantom <priyanshudebnath348@gmail.com>
2024-09-10 09:51:37 +0530

    Better Navbar Design

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -22,39 +22,39 @@ const Navlinks = [
 export default function Navbar() {
     const [activeIndex, setActiveIndex] = useState(0)
     return (
-        <header className="py-6">
-            <center>
-                <nav className="border w-1/2 rounded-[2rem] flex flex-row items-center justify-between ">
-                    <Image src="/nav-log.png" alt="logo" height={120} width={120} className="py-2 px-4"/>
-                    <div className="flex flex-row uppercase relative z-[100] ">
-                        {
-                            Navlinks.map((link, index) => {
-                                return (
-                                    <Link className="rounded-[2rem] items-center justify-center w-28 relative py-3 " key={index} href={link.link}
-                                          onClick={() => setActiveIndex(index)}><span>{link.name}</span>
-                                        {
-                                            index === activeIndex && (
-                                                <motion.div
-                                                    layoutId="navbar"
-                                                    aria-hidden="true"
-                                                    style={{
-                                                        width: "100%",
-                                                    }}
-                                                    transition={{
-                                                        type: "tween",
-                                                        duration: 0.3,
-                                                    }}
-                                                    className="bg-green-500 absolute h-full rounded-[2rem] left-0 bottom-0 -z-10"/>
-                                            )
-                                        }
-                                    </Link>
+        <header className="py-6 grid grid-cols-3">
+            <Image src="/nav-log.png" alt="logo" height={150} width={150} className="py-2 px-4 col-span-1"/>
+            <nav className="border-2 p-[1px] w-1/2 rounded-[2rem] flex flex-row items-center justify-center col-span-2">
+                <div className="flex flex-row items-center justify-evenly w-full text-center relative z-[100] ">
+                    {
+                        Navlinks.map((link, index) => {
+                            return (
+                                <Link className="rounded-[2rem] items-center justify-center relative py-3 w-full"
+                                      key={index} href={link.link}
+                                      onClick={() => setActiveIndex(index)}><span>{link.name}</span>
+                                    {
+                                        index === activeIndex && (
+                                            <motion.div
+                                                layoutId="navbar"
+                                                aria-hidden="true"
+                                                style={{
+                                                    width: "100%",
+                                                }}
+                                                transition={{
+                                                    type: "tween",
+                                                    duration: 0.3,
+                                                }}
+                                                className="bg-green-500 absolute h-full rounded-[2rem] left-0 bottom-0 -z-10"/>
+                                        )
+                                    }
+                                </Link>
 
-                                )
-                            })
-                        }
-                    </div>
-                </nav>
-            </center>
+                            )
+                        })
+                    }
+                </div>
+            </nav>
+
         </header>
     )
 }
