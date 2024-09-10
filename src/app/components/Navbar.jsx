90892461de7be1d703afb12f019f34908ce2c6ef
AriaFantom <priyanshudebnath348@gmail.com>
2024-09-10 09:26:22 +0530

    Fixed the stiffness

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,8 +24,8 @@ export default function Navbar() {
     return (
         <header className="py-6">
             <center>
-                <nav className="border w-1/2 rounded-[2rem] flex flex-row items-center justify-between px-3">
-                    <Image src="/nav-log.png" alt="logo" height={150} width={150} className=" p-3"/>
+                <nav className="border w-1/2 rounded-[2rem] flex flex-row items-center justify-between ">
+                    <Image src="/nav-log.png" alt="logo" height={120} width={120} className="py-2 px-4"/>
                     <div className="flex flex-row uppercase relative z-[100] ">
                         {
                             Navlinks.map((link, index) => {
@@ -41,10 +41,7 @@ export default function Navbar() {
                                                         width: "100%",
                                                     }}
                                                     transition={{
-                                                        type: "spring",
-                                                        bounce: 0.25,
-                                                        stiffness: 130,
-                                                        damping: 9,
+                                                        type: "tween",
                                                         duration: 0.3,
                                                     }}
                                                     className="bg-green-500 absolute h-full rounded-[2rem] left-0 bottom-0 -z-10"/>
