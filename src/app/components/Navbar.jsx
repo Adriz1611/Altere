098e5c056ef83959f30049c0da877be0cc10364d
AriaFantom <priyanshudebnath348@gmail.com>
2024-10-02 23:49:33 +0530

    PUSH EMNI

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,21 +1,35 @@
 "use client";
 
-import { useState } from "react";
+import {useState} from "react";
+import Image from "next/image";
 
 
 const Navlinks = [
-    { name: "Home", link: "/" },
-    { name: "About", link: "/" },
-    { name: "Impact", link: "/" },
-    { name: "Metrics", link: "/" },
+    {name: "Home", link: "/"},
+    {name: "About", link: "/"},
+    {name: "Food Recycling", link: "/"},
+    {name: "Feed Manufactures", link: "/"},
+    {name: "Impact", link: "/"},
 ];
 
 export default function Navbar() {
     const [active, setActive] = useState(false);
 
     return (
-        <header>
-
+        <header className="shadow-[0px_10px_10px_0px_rgba(0,_0,_0,_0.22)] w-full fixed z-10 bg-white">
+            <nav className="flex flex-row justify-between items-center px-4 py-4">
+                <Image src="/nav-log.png" alt="AlteRe Logo" width={130} height={0}/>
+                <ul className="flex flex-row gap-5 font-main items-center">
+                    {Navlinks.map((link) => (
+                        <li key={link.name}>
+                            <a className="text-xl font-medium" href={link.link}>{link.name}</a>
+                        </li>
+                    ))}
+                    <button className="bg-colors-secondary-500 py-3 px-5 rounded-xl text-white font-medium">
+                        Contact Us
+                    </button>
+                </ul>
+            </nav>
         </header>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,12 @@
 
 import Image from "next/image";
 import React from "react";
-import { motion } from "framer-motion";
+import {motion} from "framer-motion";
 
 export default function Home() {
-  return (
-    <div>
-
-    </div>
-  );
+    return (<main className="py-32">
+        <section id="hero" className="flex flex-row justify-between px-20 py-20">
+            <h1 className="text-[4rem] font-medium w-2/4 font-main ">Waste is a Resource in the wrong place</h1>
+        </section>
+    </main>);
 }
