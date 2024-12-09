f64d40f118d5085539627f8e614f93334a606b44
Adriz1611 <adrijbhadra@gmail.com>
2024-12-09 21:37:59 +0530

    ...

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import "./globals.css";
 import { Ubuntu } from "next/font/google";
 import { Jost } from "next/font/google";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const ubuntu = Ubuntu({
   weight: ["300", "400", "500", "700"],
@@ -24,7 +26,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${ubuntu.variable} ${jost.variable}`}>
+        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
